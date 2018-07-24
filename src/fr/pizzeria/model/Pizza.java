9324fd014f342adfb2b8f87728e0751710f2a94a
Treille Morgane <treillemorgane@gmail.com>
2018-07-24 11:58:56 +0200

    nouveau package et déplacement de la classe Pizza

---
+++ src/fr/pizzeria/model/Pizza.java
@@ -0,0 +1,21 @@
+package fr.pizzeria.model;
+
+public class Pizza {
+	int id;
+	String code;
+	String libelle;
+	double prix;
+	
+	public Pizza(String code,String libelle,double prix){
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+	
+	public Pizza(int id,String code,String libelle,double prix){
+		this.id = id;
+		this.code = code;
+		this.libelle = libelle;
+		this.prix = prix;
+	}
+}
